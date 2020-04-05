0d1a169775383b7e3fe53ed75cd9976792d867de
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2020-04-05 19:07:45 -0400

    Full reset

--- api/docker/db/mongoinit.sh
+++
@@ -1,2 +0,0 @@
-echo "Hello Mongo!"
-mongoimport --host 127.0.0.1 --port 27017 --db gradual --collection logs --type json --file /docker-entrypoint-initdb.d`/sampleData.json
--- api/docker/dockerrun.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-docker run -v `pwd`/db:/docker-entrypoint-initdb.d/:ro -p 27017:27017 mongo
