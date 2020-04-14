150678d0374bb4155dbcc077ef1bae49664c27af
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2020-04-14 01:03:04 -0400

    Fix basic layout

--- gradual/App.tsx
+++ gradual/App.tsx
@@ -1,13 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
 import {SafeAreaView, StyleSheet, View, Text, StatusBar} from 'react-native';
 
@@ -22,76 +12,46 @@ const App = () => {
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <View style={styles.header}>
-          <Text style={styles.sectionTitle}>Thursday, April 9th</Text>
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <Text style={styles.sectionTitle}>Thursday, April 9th</Text>
+          </View>
+          <View style={styles.initialTip}>
+            <Text style={styles.sectionTitle}>Welcome to Gradual!</Text>
+            <Text style={styles.sectionTitle}>Add a task to get started.</Text>
+          </View>
+          <View style={styles.addButton}>
+            <AddButton />
+          </View>
         </View>
-        <View style={styles.initialTip}>
-          <Text style={styles.sectionTitle}>Welcome to Gradual!</Text>
-          <Text style={styles.sectionTitle}>Add a task to get started.</Text>
-        </View>
-        <AddButton />
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  header: {
+  container: {
     display: 'flex',
-    paddingHorizontal: 24,
-    justifyContent: 'center',
     alignItems: 'center',
+    height: '100%',
+    justifyContent: 'space-between',
+  },
+  header: {
+    flex: 1,
   },
   initialTip: {
-    display: 'flex',
-    paddingHorizontal: 24,
-    justifyContent: 'center',
     alignItems: 'center',
+    flex: 1,
   },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
     color: Colors.black,
   },
+  addButton: {
+    width: '100%',
+    alignItems: 'center',
+  },
 });
-
-// const styles = StyleSheet.create({
-//   scrollView: {
-//     backgroundColor: Colors.lighter,
-//   },
-//   engine: {
-//     position: 'absolute',
-//     right: 0,
-//   },
-//   body: {
-//     backgroundColor: Colors.white,
-//   },
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//     color: Colors.black,
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//     color: Colors.dark,
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-//   footer: {
-//     color: Colors.dark,
-//     fontSize: 12,
-//     fontWeight: '600',
-//     padding: 4,
-//     paddingRight: 12,
-//     textAlign: 'right',
-//   },
-// });
 
 export default App;
